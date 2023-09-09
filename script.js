4d146eccb60c5ff60475c52567f0512a4b468f31
jng69 <142822909+jng69@users.noreply.github.com>
2023-09-09 02:15:48 -0400

    Update script.js
    Added image preloader for the 360 panoramic

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ productContainer.addEventListener('touchend', endDrag);
 
 // Add a click event to start the auto-rotation animation
 //productContainer.addEventListener('click', startAutoRotation);
-startAutoRotation();
+//startAutoRotation();
 
 // Add slider change event
 rotationSlider.addEventListener('input', () => {
@@ -75,3 +75,73 @@ rotationSlider.addEventListener('input', () => {
     const imageURL = `product/${rotationValue}.jpg`;
     productImage.src = imageURL;
 });
+
+/* PRELOAD IMAGES */
+var imageUrls = [
+'https://jng69.github.io/360productviewer/product/0.jpg',
+'https://jng69.github.io/360productviewer/product/1.jpg',
+'https://jng69.github.io/360productviewer/product/2.jpg',
+'https://jng69.github.io/360productviewer/product/3.jpg',
+'https://jng69.github.io/360productviewer/product/4.jpg',
+'https://jng69.github.io/360productviewer/product/5.jpg',
+'https://jng69.github.io/360productviewer/product/6.jpg',
+'https://jng69.github.io/360productviewer/product/7.jpg',
+'https://jng69.github.io/360productviewer/product/8.jpg',
+'https://jng69.github.io/360productviewer/product/9.jpg',
+'https://jng69.github.io/360productviewer/product/10.jpg',
+'https://jng69.github.io/360productviewer/product/11.jpg',
+'https://jng69.github.io/360productviewer/product/12.jpg',
+'https://jng69.github.io/360productviewer/product/13.jpg',
+'https://jng69.github.io/360productviewer/product/14.jpg',
+'https://jng69.github.io/360productviewer/product/15.jpg',
+'https://jng69.github.io/360productviewer/product/16.jpg',
+'https://jng69.github.io/360productviewer/product/17.jpg',
+'https://jng69.github.io/360productviewer/product/18.jpg',
+'https://jng69.github.io/360productviewer/product/19.jpg',
+'https://jng69.github.io/360productviewer/product/20.jpg',
+'https://jng69.github.io/360productviewer/product/21.jpg',
+'https://jng69.github.io/360productviewer/product/22.jpg',
+'https://jng69.github.io/360productviewer/product/23.jpg',
+'https://jng69.github.io/360productviewer/product/24.jpg',
+'https://jng69.github.io/360productviewer/product/25.jpg',
+'https://jng69.github.io/360productviewer/product/26.jpg',
+'https://jng69.github.io/360productviewer/product/27.jpg',
+'https://jng69.github.io/360productviewer/product/28.jpg',
+'https://jng69.github.io/360productviewer/product/29.jpg',
+'https://jng69.github.io/360productviewer/product/30.jpg',
+'https://jng69.github.io/360productviewer/product/31.jpg',
+'https://jng69.github.io/360productviewer/product/32.jpg',
+'https://jng69.github.io/360productviewer/product/33.jpg',
+'https://jng69.github.io/360productviewer/product/34.jpg'
+];
+
+function preloadImages(urls, callback) {
+    var loaded = 0;
+    var images = [];
+
+    function imageLoaded() {
+        loaded++;
+        if (loaded === urls.length) {
+            callback(images);
+            console.log(images);
+        }
+    }
+
+    for (var i = 0; i < urls.length; i++) {
+        var img = new Image();
+        img.src = urls[i];
+        img.onload = imageLoaded;
+        images.push(img);
+    }
+}
+
+preloadImages(imageUrls, function(images) {
+    /* All images are preloaded and stored in the "images" array You can now call your function or perform any action you want with the preloaded images. */
+    myFunction(images);
+});
+
+function myFunction(images) {
+    /* Your code to display or manipulate the preloaded images goes here. */
+    console.log('all images loaded');
+    startAutoRotation();
+}
